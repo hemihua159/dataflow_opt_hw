f178b72bfcd5d5584f2b16ea34dfac0f702ebefb
hemihua159 <65721009+hemihua159@users.noreply.github.com>
2020-06-13 08:48:38 +0800

    Add files via upload

---
+++ dataflow_opt_hw/Main.scala
@@ -0,0 +1,97 @@
+import java.util.{Properties, UUID}
+import java.util.Properties
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010
+
+import com.bingocloud.{ClientConfiguration, Protocol}
+import com.bingocloud.auth.BasicAWSCredentials
+import com.bingocloud.services.s3.AmazonS3Client
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.nlpcn.commons.lang.util.IOUtil
+
+object Main {
+  //s3参数
+  val accessKey = "4C1D5EF21C436099F7AA"
+  val secretKey = "WzlBQjA2Q0VGNTFBRTU0RDYzMEE5NDVBOUY1RDY0NEVCRENFNjczRUJd"
+  val endpoint = "scuts3.depts.bingosoft.net:29999"
+  val bucket = "wordcount"
+
+  //上传文件的路径前缀
+  val keyPrefix = "upload/"
+  //上传数据间隔 单位毫秒
+  val period = 5000
+  //要读取的文件
+  val key = "daas.txt"
+   /**
+   * 输入的主题名称
+   */
+  val inputTopic = "daas_himihua"
+  //kafka参数
+  val topic = "daas_himihua"
+
+
+
+  val bootstrapServers = "bigdata35.depts.bingosoft.net:29035,bigdata36.depts.bingosoft.net:29036,bigdata37.depts.bingosoft.net:29037"
+
+  def main(args: Array[String]): Unit = {
+    val s3Content = readFile()
+    produceToKafka(s3Content)
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val kafkaProperties = new Properties()
+    kafkaProperties.put("bootstrap.servers", bootstrapServers)
+    kafkaProperties.put("group.id", UUID.randomUUID().toString)
+    kafkaProperties.put("auto.offset.reset", "earliest")
+    kafkaProperties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    kafkaProperties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    val kafkaConsumer = new FlinkKafkaConsumer010[String](inputTopic,
+      new SimpleStringSchema, kafkaProperties)
+    kafkaConsumer.setCommitOffsetsOnCheckpoints(true)
+    val inputKafkaStream = env.addSource(kafkaConsumer)
+    inputKafkaStream.map(x => println(x))
+    inputKafkaStream.writeUsingOutputFormat(new S3Writer(accessKey, secretKey, endpoint, bucket, keyPrefix, period))
+    env.execute()
+  }
+
+  /**
+   * 从s3中读取文件内容
+   *
+   * @return s3的文件内容
+   */
+  def readFile(): String = {
+    val credentials = new BasicAWSCredentials(accessKey, secretKey)
+    val clientConfig = new ClientConfiguration()
+    clientConfig.setProtocol(Protocol.HTTP)
+    val amazonS3 = new AmazonS3Client(credentials, clientConfig)
+    amazonS3.setEndpoint(endpoint)
+    val s3Object = amazonS3.getObject(bucket, key)
+    IOUtil.getContent(s3Object.getObjectContent, "UTF-8")
+  }
+
+  /**
+   * 把数据写入到kafka中
+   *
+   * @param s3Content 要写入的内容
+   */
+  def produceToKafka(s3Content: String): Unit = {
+    val props = new Properties
+    props.put("bootstrap.servers", bootstrapServers)
+    props.put("acks", "all")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    val producer = new KafkaProducer[String, String](props)
+    val dataArr = s3Content.split("\n")
+    for (s <- dataArr) {
+      if (!s.trim.isEmpty) {
+        val record = new ProducerRecord[String, String](topic, null, s)
+        println("开始生产数据：" + s)
+        producer.send(record)
+      }
+    }
+    producer.flush()
+    producer.close()
+  }
+
+
+}
